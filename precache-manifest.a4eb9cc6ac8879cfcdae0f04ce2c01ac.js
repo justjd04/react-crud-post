3d163e5420bc227d6e389d0101e1c240f6e6632c
Jan Dohinog <janreydohinog@gmail.com>
2021-07-11 07:11:40 +0800

    Updates

---
+++ precache-manifest.a4eb9cc6ac8879cfcdae0f04ce2c01ac.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "84e31f8dc45f1f5b12c4288f10163018",
+    "url": "/react-crud-post/index.html"
+  },
+  {
+    "revision": "8c45c44192eacde21972",
+    "url": "/react-crud-post/static/css/main.d214b88c.chunk.css"
+  },
+  {
+    "revision": "5d64d2ff8179bc0a3473",
+    "url": "/react-crud-post/static/js/2.94068da4.chunk.js"
+  },
+  {
+    "revision": "8c45c44192eacde21972",
+    "url": "/react-crud-post/static/js/main.f2be30ea.chunk.js"
+  },
+  {
+    "revision": "34a2e33d1e40ab0ab948",
+    "url": "/react-crud-post/static/js/runtime-main.93769148.js"
+  }
+]);
